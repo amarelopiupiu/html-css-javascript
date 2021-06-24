4e95a66056cb3cf06ccf724d4d81c7ba326dc960
FernandaMakiHirose <femahi2020@gmail.com>
2021-06-24 18:05:55 -0300

    Novo commit

---
+++ exercicios-javascript/variaveis/variaveis.js
@@ -0,0 +1,29 @@
+// string = é um texto
+let minhaString = 'Fernanda Maki Hirose'
+
+// number
+let meuNumeroInteiro = 4
+let meuNumeroFloat = 4.5
+
+// boolean
+let meuBooleanTrue = true
+let meuBooleanFalse = false
+
+// array
+let meuArrayNumber = [1, 2, 3, 4, 5];
+let meuArrayString = ["Olá", "mundo"];
+let meuArrayDiverso = ["Fernanda", 1, "Maki", 2, "Hirose"];
+
+// acessar os valores do array
+meuArrayString[0];
+
+// object 
+let pessoa = {
+    nome: "Fernanda",
+    idade: 18,
+    pais: "Brasil"
+};
+
+// acessar dados do object 
+pessoa.nome;
+pessoa.idade;
